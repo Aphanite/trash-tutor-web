3689f67ac1a5e754c6d94b767dbd382de8f71ea9
Aphanite <ena.rajovic@online.de>
2024-02-15 02:03:12 +0100

    WIP: Adjust LandingPage

---
+++ src/components/LandingPage/LandingPage.tsx
@@ -0,0 +1,36 @@
+import { useKey } from '../../hooks/useKey'
+import styles from './LandingPage.module.css'
+import leaves from '../../assets/images/leaves.png'
+
+export function LandingPage({ onNavigate }: { onNavigate: any }) {
+  const apiKey = useKey()
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <img className={styles.logo} src={leaves} />
+        <h2 className={styles.title}>
+          Let's talk <span>trash!</span>
+        </h2>
+      </div>
+      <div className={styles.textContainer}>
+        {!false && (
+          <p className={`${styles.text} ${styles.optionalText}`}>
+            Baffled by bottles and puzzled by plastics?
+          </p>
+        )}
+        <p className={styles.text}>
+          Trash Tutor, your friendly AI helper, shows you how to sort your recyclables.
+        </p>
+      </div>
+      <button
+        className={styles.button}
+        onClick={() => {
+          console.log(apiKey ? 'Key provided' : 'No key provided')
+        }}
+      >
+        Start
+      </button>
+    </div>
+  )
+}
