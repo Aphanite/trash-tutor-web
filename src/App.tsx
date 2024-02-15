35105e8d92e851a275160818b8d6b35e8a07bb42
Aphanite <ena.rajovic@online.de>
2024-02-15 02:01:40 +0100

    Create routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { ErrorPage } from './components/ErrorPage'
+import { SuccessPage } from './components/SuccessPage'
+import { ImagePreview } from './components/ImagePreview'
+import { Camera } from './components/Camera'
+import { CameraPermissionPage } from './components/CameraPermissionPage'
+import { LandingPage } from './components/LandingPage/LandingPage'
+import { ApiKeyPage } from './components/ApiKeyPage'
+
+type PageKeys =
+  | 'landing-page'
+  | 'camera-permission'
+  | 'camera'
+  | 'api-key-form'
+  | 'image-preview'
+  | 'success'
+  | 'error'
+
+const PAGES: Record<PageKeys, any> = {
+  'landing-page': LandingPage,
+  'camera-permission': CameraPermissionPage,
+  camera: Camera,
+  'api-key-form': ApiKeyPage,
+  'image-preview': ImagePreview,
+  success: SuccessPage,
+  error: ErrorPage,
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentPage, setCurrentPage] = React.useState<PageKeys>('landing-page')
+
+  const Page = PAGES[currentPage]
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Page
+      onNavigate={(page: PageKeys) => {
+        setCurrentPage(page)
+      }}
+    />
   )
 }
 
