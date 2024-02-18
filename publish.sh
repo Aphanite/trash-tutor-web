93ad7fb2c03b17497c354df693eb9703eb368d73
Aphanite <ena.rajovic@online.de>
2024-02-19 00:24:24 +0100

    Create publish script

---
+++ publish.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+# create fresh build
+rm -rf dist
+yarn build
+
+DATE=$(date -u +%Y-%m-%dT%H:%M)
+
+cd dist
+
+# create CNAME file for custom domain
+echo "trash-tutor.aphanite.net" > CNAME
+
+git init
+git add .
+git commit -m "new export at $DATE"
+git remote add origin git@github.com:Aphanite/trash-tutor-pages.git
+git push -u origin main --force
