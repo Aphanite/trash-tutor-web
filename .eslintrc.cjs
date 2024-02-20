4c5c24e791159d3055c06667805eae35a632b9a5
Zsolt Ero <zsolt.ero@gmail.com>
2024-02-20 03:06:33 +0100

    lint, no strict

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,5 +14,8 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/ban-types": "warn",
+    "@typescript-eslint/no-unused-vars": "warn",
   },
 }
