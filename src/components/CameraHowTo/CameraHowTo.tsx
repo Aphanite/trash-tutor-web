5185c048565a3098e39711ba3dd03a76fd0e2038
Aphanite <ena.rajovic@online.de>
2024-02-26 18:34:29 +0100

    Add Camera-How-to page

---
+++ src/components/CameraHowTo/CameraHowTo.tsx
@@ -0,0 +1,32 @@
+import styles from './CameraHowTo.module.css'
+import camera from '../../assets/images/camera.png'
+import { usePage } from '../../providers/PageProvider'
+import { ArrowDown } from 'react-feather'
+
+export function CameraHowTo() {
+  const { navigate } = usePage()
+  return (
+    <div className={`container ${styles.container}`}>
+      <div className={styles.imageContainer}>
+        <img style={{ width: '30%' }} src={camera} />
+        <h2 className={styles.title}>How it works</h2>
+      </div>
+      <div className={styles.textContainer}>
+        <div className={styles.steps}>
+          <p>Allow Camera access</p>
+          <ArrowDown size={20} />
+          <p>
+            Snap a clear picture of <strong>one item</strong>
+            <br />
+            on a plain background
+          </p>
+          <ArrowDown size={20} />
+          <p>Confirm and scan!</p>
+        </div>
+        <button className="btn primary" onClick={() => navigate('camera')}>
+          Open Camera
+        </button>
+      </div>
+    </div>
+  )
+}
