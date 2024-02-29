4b15e44ce98e8b6146a438df0f55bea0cdb76b7d
Aphanite <ena.rajovic@online.de>
2024-02-29 23:59:32 +0100

    Adjust convertHelper for node.js

--- docs/upload-error/convertHelper.js
+++ docs/upload-error/convertHelper.js
@@ -1,14 +1,21 @@
+import fs from 'fs'
+
 // './test-img/test.JPG'
 export function convertToBase64(filePath) {
   const img = fs.readFileSync(filePath)
   const base64String = Buffer.from(img).toString('base64')
-  return 'data:image/jpeg;base64,' + base64String
+
+  return 'data:image/png;base64,' + base64String
 }
 
-export function convertToImage(base64) {
+export async function convertToImage(base64) {
   let [metaData, data] = base64.split(';')
 
   let contentType = metaData.split(':')[1]
+
+  console.log('metaData', metaData)
+  console.log('contentType', metaData.split(':'))
+  console.log('contentType', metaData.split(':')[1])
   let imageExt = contentType.split('/')[1]
 
   // get the real base64 content of the file
@@ -16,8 +23,13 @@ export function convertToImage(base64) {
 
   // convert to blob
   let blob = b64toBlob(realData, contentType)
+  const arrayBuffer = await blob.arrayBuffer()
+  const buffer = Buffer.from(arrayBuffer)
 
-  return { contentType, image: new File([blob], `photo.${imageExt}`) }
+  const fileName = `photo.${imageExt}`
+  fs.writeFileSync(fileName, buffer)
+
+  return { contentType, fileName }
 }
 
 function b64toBlob(b64Data, contentType, sliceSize = 512) {
