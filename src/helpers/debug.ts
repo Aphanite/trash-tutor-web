f58ab7b3373132627f3722bc6e3502ecd4b3d175
Aphanite <ena.rajovic@online.de>
2024-03-02 23:55:41 +0100

    Upload json

--- src/helpers/debug.ts
+++ src/helpers/debug.ts
@@ -1,17 +1,22 @@
-export async function uploadToBucket(base64: string, code: string) {
-  try {
-    const { image, contentType } = convertToImage(base64)
+import { WasteCategory } from './types'
 
+export async function uploadToBucket(data: {
+  uri: string
+  code: string
+  categories: WasteCategory[]
+  location: string | null
+}) {
+  try {
     // const url = `https://worker.trashtutor.com/upload_error/${code}`
 
     // for development - connect to local worker
-    const url = `https://localhost:8787/upload_error/${code}`
+    const url = `https://localhost:8787/upload_error`
 
     const response = await fetch(url, {
       method: 'PUT',
-      body: image,
+      body: JSON.stringify(data),
       headers: {
-        'Content-Type': contentType,
+        'Content-Type': 'application/json',
       },
     })
 
@@ -19,40 +24,4 @@ export async function uploadToBucket(base64: string, code: string) {
   } catch (error) {
     console.error('Error uploading file: ', error)
   }
-}
-
-function convertToImage(base64: string) {
-  let [metaData, data] = base64.split(';')
-
-  let contentType = metaData.split(':')[1]
-  let imageExt = contentType.split('/')[1]
-
-  // get the real base64 content of the file
-  let realData = data.split(',')[1]
-
-  // convert to blob
-  let blob = b64toBlob(realData, contentType)
-
-  return { contentType, image: new File([blob], `photo.${imageExt}`) }
-}
-
-function b64toBlob(b64Data: string, contentType: string, sliceSize = 512) {
-  let byteCharacters = atob(b64Data)
-  let byteArrays = []
-
-  for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-    let slice = byteCharacters.slice(offset, offset + sliceSize)
-
-    let byteNumbers = new Array(slice.length)
-    for (let i = 0; i < slice.length; i++) {
-      byteNumbers[i] = slice.charCodeAt(i)
-    }
-
-    let byteArray = new Uint8Array(byteNumbers)
-
-    byteArrays.push(byteArray)
-  }
-
-  let blob = new Blob(byteArrays, { type: contentType })
-  return blob
 }
