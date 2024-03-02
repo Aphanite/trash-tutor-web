d625bfb19733913fd165485225ef08c4b7e1ed98
Aphanite <ena.rajovic@online.de>
2024-03-02 17:12:53 +0100

    Update request

--- src/helpers/debug.ts
+++ src/helpers/debug.ts
@@ -1,26 +1,22 @@
-export async function uploadToBucket(base64: string, code: string, object?: string) {
+export async function uploadToBucket(base64: string, code: string) {
   try {
     const { image, contentType } = convertToImage(base64)
 
-    const timestamp = new Date().toISOString().replace(/[-:.Z]/g, '')
+    const url = `https://worker.trashtutor.com/upload_error/${code}`
 
-    // for production - connect to deployed Cloudflare worker
-    // const production_url = `https://worker.trashtutor.com/upload_error_${timestamp}`
-
-    // for development - connect to local worker (don't forget to start it)
-    const url = `http://192.168.1.45:8787/upload_error_${timestamp}`
+    // for development - connect to local worker
+    // const url = `https://localhost:8787/upload_error/${code}`
 
     const response = await fetch(url, {
       method: 'PUT',
-      body: JSON.stringify({ image, code, object }),
+      body: image,
       headers: {
         'Content-Type': contentType,
       },
     })
-    // Log the response
+
     console.log('File uploaded successfully: ', response)
   } catch (error) {
-    // Log any errors
     console.error('Error uploading file: ', error)
   }
 }
