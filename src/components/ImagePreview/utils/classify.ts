df8979520595fb7f1643f0cfb85e79128440129d
Aphanite <ena.rajovic@online.de>
2024-03-08 23:26:01 +0000

    Test dummy response

--- src/components/ImagePreview/utils/classify.ts
+++ src/components/ImagePreview/utils/classify.ts
@@ -9,8 +9,20 @@ export async function classify(
   key: string,
 ) {
   try {
-    const llmAnswer = await classifyImage(uri, location, wasteCategories, key)
-    return await convertAnswer(llmAnswer, key)
+    // const llmAnswer = await classifyImage(uri, location, wasteCategories, key)
+    // const json = await convertAnswer(llmAnswer, key)
+    // console.log('json', json)
+    return {
+      status: 'success',
+      data: {
+        itemDetected: true,
+        item: 'yellow plastic container',
+        categoryDetected: true,
+        categoryName: 'packaging',
+        reason:
+          'The container is made of plastic and used for transporting mail or packages, and should be recycled in the packaging category if clean and free of hazardous materials.',
+      },
+    }
   } catch (error) {
     console.log(Object.entries(error))
     console.error('Error when classifying: ', error)
