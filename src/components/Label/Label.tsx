a5a35d04ae5fffe90a207bee99b4f136c0e59f2a
Aphanite <ena.rajovic@online.de>
2024-03-10 14:29:49 +0100

    Split Result/CategoryInfo

--- src/components/Label/Label.tsx
+++ src/components/Label/Label.tsx
@@ -1,17 +1,27 @@
 import { WasteColors } from '../../helpers/types'
 import styles from './Label.module.css'
-import { Trash2 } from 'react-feather'
+import { ChevronRight, Trash2 } from 'react-feather'
 
-export function Label({ color, children }: { color: WasteColors | 'dark-grey'; children: any }) {
+export function Label({
+  color,
+  children,
+  onClick,
+}: {
+  color: WasteColors | 'dark-grey'
+  children: any
+  onClick: () => void
+}) {
   return (
     <div
       className={styles.label}
       style={{
         backgroundColor: `var(--${color})`,
       }}
+      onClick={onClick}
     >
       <Trash2 size={16} color="#f8faed" />
       <p className={styles.labelText}>{children}</p>
+      <ChevronRight size={16} color="#f8faed" />
     </div>
   )
 }
--- src/components/SuccessPage/SuccessPage.tsx
+++ src/components/SuccessPage/SuccessPage.tsx
@@ -28,41 +28,96 @@ function SuccessPage({ object, categoryName, uri }: SuccessParams) {
 
   const binColor = chosenCategory?.binColor === 'null' ? 'dark-grey' : chosenCategory?.binColor
 
-  const text =
+  let text =
     'The container is made of plastic and used for transporting mail or packages, and should be recycled in the packaging category if clean and free of hazardous materials.'
-  const displayText = isMaximised ? text : `${text.substring(0, 120)}...`
+
+  text =
+    'It is a glass packaging item typically used for food or beverages and is not a non-recyclable type like window or mirror glass.'
 
   function capitalize(string: string) {
     return string[0].toUpperCase() + string.substring(1)
   }
 
+  const Result = () => {
+    return (
+      <>
+        <div className={styles.textContainer}>
+          <p className={styles.locationInfo}>How to recycle in {location || 'general'}</p>
+          <h2>{capitalize(object)}</h2>
+
+          <Label color={binColor} onClick={() => setIsMaximised(true)}>
+            {categoryName.toLowerCase()}
+          </Label>
+        </div>
+
+        <p>{text}</p>
+
+        <div className={styles.buttonContainer}>
+          <button
+            className="btn secondary"
+            onClick={() => {
+              navigate('camera')
+            }}
+          >
+            Scan more
+          </button>
+        </div>
+      </>
+    )
+  }
+
+  const CategoryExplained = () => {
+    return (
+      <>
+        <div className={styles.textContainer}>
+          <p className={styles.locationInfo}>How to recycle in {location || 'general'}</p>
+          <h2>
+            Category:{' '}
+            <span style={{ color: `var(--${binColor})` }}>{capitalize(categoryName)}</span>
+          </h2>
+        </div>
+
+        <div className={styles.categoryInfoContainer}>
+          <div>
+            <p className={styles.question}>What belongs in it?</p>
+            <p>{chosenCategory.userDescription || chosenCategory.description}</p>
+          </div>
+
+          <div>
+            <p className={styles.question}>Where can I dispose of it?</p>
+            <div className={styles.disposalInfo}>
+              <MapPin size={16} style={{ color: `var(--${binColor})` }} />
+              <p>
+                {chosenCategory?.domestic ? 'At domestic bin' : 'At drop-off or collection point'}
+              </p>
+            </div>
+          </div>
+
+          <p className={styles.disclaimer}>Please note: For general guidance only.</p>
+        </div>
+
+        <div className={styles.buttonContainer}>
+          <button
+            className="btn secondary"
+            style={{
+              color: `var(--${binColor})`,
+              borderColor: `var(--${binColor})`,
+            }}
+            onClick={() => {
+              setIsMaximised(false)
+            }}
+          >
+            Back to result
+          </button>
+        </div>
+      </>
+    )
+  }
+
+  const content = isMaximised ? <CategoryExplained /> : <Result />
   return (
     <ResultPage uri={uri} maximised={isMaximised}>
-      <div className={styles.textContainer}>
-        <p className={styles.locationInfo}>How to recycle in {location || 'general'}</p>
-        <h2>{capitalize(object)}</h2>
-
-        <Label color={binColor}>{categoryName.toLowerCase()}</Label>
-      </div>
-
-      <div className={styles.infoContainer}>
-        <p className={`${isMaximised && styles.active}`}>{text}</p>
-        <button className={styles.readBtn} onClick={() => setIsMaximised(current => !current)}>
-          {isMaximised ? 'Read less' : 'Read more'}
-        </button>
-      </div>
-
-      <p className={`${styles.disclaimer}`}>Please note: For general guidance only.</p>
-      <div className={styles.buttonContainer}>
-        <button
-          className="btn secondary"
-          onClick={() => {
-            navigate('camera')
-          }}
-        >
-          Scan more
-        </button>
-      </div>
+      {content}
     </ResultPage>
   )
 }
